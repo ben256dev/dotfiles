8ec9340e8be886df064544e74c0496cb869b2260
ben256dev <benjamin@ben256.com>
2026-05-21 13:02:10 -0600

    v ~ for history of opened files

--- .config/shell/v.sh
+++ .config/shell/v.sh
@@ -1,5 +1,7 @@
 # Prefer executable v script when present
 if [ -x "$HOME/.local/bin/v" ]; then
+  unalias v >/dev/null 2>&1 || true
+  unset -f v >/dev/null 2>&1 || true
   v() {
     "$HOME/.local/bin/v" "$@"
   }
