d23983366381775243cfcf9ea514c48ba7e5980a
ben256dev <benjamin@ben256.com>
2026-08-08 06:46:37 -0600

    v can warn if file dne

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -246,5 +246,6 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.expandtab = false
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
   end,
 })
